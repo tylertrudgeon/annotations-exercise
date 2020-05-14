fe0910d4577ffd7d210af05d132d5edcc3f04c1d
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-14 12:03:20 -0500

    finished exercise for better fix

--- src/Author.java
+++ src/Author.java
@@ -2,25 +2,24 @@
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 import java.util.ArrayList;
-import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private ArrayList<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
-    public List<String> getBooks() {
+    public ArrayList<String> getBooks() {
         return books;
     }
 
-    public List<String> publishedBooks() {
+    public ArrayList<String> publishedBooks() {
         return books;
     }
 
@@ -28,7 +27,6 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
